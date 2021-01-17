137492dbe9ecf939977b323485fecb8fb3916d17
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-01-17 23:35:52 +0530

    Added additional util methods for fetching user and guild member

--- src/bot/bot_utils.py
+++ src/bot/bot_utils.py
@@ -13,6 +13,24 @@ def generate_embed(title: str, author: discord.Member, *, description: str, colo
     ).set_footer(text=author.display_name, icon_url=author.avatar_url)
 
 
+async def fetch_guild_member(guild: discord.Guild, user: Union[discord.Member, int]):
+    if not isinstance(user, discord.Member):
+        try:
+            return await guild.fetch_member(int(user))
+        except (discord.Forbidden, discord.HTTPException):
+            return
+    return user
+
+
+async def fetch_user(bot: discord.ext.commands.Bot, user: Union[discord.Member, int]):
+    if not isinstance(user, discord.Member):
+        try:
+            return await bot.fetch_user(int(user))
+        except (discord.Forbidden, discord.HTTPException):
+            return
+    return user
+
+
 async def emoji_selection_detector(ctx: Context, emoji_list: List[Union[discord.Emoji, discord.PartialEmoji, str]],
                                    embed: discord.Embed = None, wait_for: int = 30, *, message_content: str = None,
                                    show_reject: bool = True) -> Union[None, discord.Emoji, discord.PartialEmoji, str]:
